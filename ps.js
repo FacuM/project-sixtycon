60ddde617093f8ddbfd00565504f98d1f2636e84
FacuM <facumo.fm@gmail.com>
2018-03-22 20:24:23 -0300

    select the message field and make it a variable

--- ps.js
+++ ps.js
@@ -1,3 +1,6 @@
 var script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.3.1.min.js';
 document.body.appendChild(script);
+document.addEventListener('DOMContentLoaded', function() {
+ var message = $('._2S1VP').html();
+}, true);
